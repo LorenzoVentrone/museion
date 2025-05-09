6e464c5e9ba69057fd2f1456ad7ff250e04f2a00
LucaTamSapienza <tutordimatematica.ing@gmail.com>
2025-05-09 12:48:26 +0200

    fixing sky

--- frontend/src/components/environment/Sky.jsx
+++ frontend/src/components/environment/Sky.jsx
@@ -3,9 +3,9 @@ import * as THREE from 'three'
 const Sky = () => {
   return (
     <>  
-        {/* In Pratica è tutto contenuto dentor una sfera, che da l'impressione di essere un cielo */}
+        {/* In Pratica è tutto contenuto dentro una sfera, che da l'impressione di essere un cielo */}
         <mesh>
-            <sphereGeometry args={[100, 100, 32, 32]} />
+            <sphereGeometry args={[1000, 1000, 32, 32]} />
             <meshStandardMaterial color="skyblue" side={THREE.DoubleSide}/> {/* royalBlue */}
         </mesh>
     </>
